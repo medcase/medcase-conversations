fdc3e06d6eb8d37e9f046447b4d85d2fef55978b
Kamil Ziemann ARC <kamil.ziemann@arcsoftware.it>
2023-08-16 15:25:43 +0200

    Added functions & utils for medcase conversations

---
+++ src/functions/index.ts
@@ -0,0 +1,5 @@
+export { onClientEvent, removeClientEventListener } from './clientEvent';
+export { onConversationEvent, removeConversationEventListener } from './conversationEvent';
+export { default as getMedcaseMessages } from './getMedcaseMessages';
+export { default as sendMedcaseMessage } from './sendMedcaseMessage';
+export { default as getMedcaseConversation } from './getMedcaseConversation';
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { convertMessageClass, convertPaginatorMessages } from './convert';
